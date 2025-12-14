c365fd72424fb86f6f11b37dc8baccafb6371db2
Egor Merkushev <gorkaedeep@gmail.com>
2025-12-14 13:26:52 +0300

    fix ios support

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "docc2context",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v13),
+        .iOS(.v13)
     ],
     products: [
         .library(
